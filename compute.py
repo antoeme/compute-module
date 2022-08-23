6425932406ad907590b09aac7c6603830ed5a31f
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-08-23 17:06:16 +0200

    sistemato metodo post per il db

--- compute.py
+++ compute.py
@@ -9,19 +9,17 @@ from os import getenv
 
 NUM_SENSORI = 4
 URL_QUERY = "http://127.0.0.1:5003/query"
-DATI = []
+
 
 # load environment variables from '.env' file
 load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def helloworld():
-    return jsonify({"about": " Helloworld !"})
 
 @app.route('/calcoli', methods=[ 'GET','POST'])
 def calcoli():
+    d = []
     for id in range(NUM_SENSORI):
         row = requests.get(URL_QUERY+'/'+str(id+1))
         riga = json.dumps(row.json())
@@ -38,7 +36,7 @@ def calcoli():
         mean = sum(a) / len(a)
         var = sum((l-mean)**2 for l in a) / len(a)
         st_dev = math.sqrt(var)
-        
+
         print("Statistiche su sensore: ",id+1)
         print("Standard deviation of the given list: " + str(st_dev))
         print("media = ", mean)
@@ -47,20 +45,15 @@ def calcoli():
         print("max = ", massimo)
 
         dati = {"id_sensore":id+1, "min":minimo, "max":massimo, "media":mean, "devs":st_dev }
-        DATI.append(dati)
+        d.append(dati)
+        
+    r = requests.post("http://127.0.0.1:5003/statistiche", json = d)
     
-    print(DATI)
 
     
     return 'eseguiti calcoli su elementi'
 
 
-@app.route('/transfer')
-def transfer():
-    # if len(DATI) == 0:
-    #     return "errore, dati non presenti in memoria"
-    # else:
-        return (json.dumps(DATI[-1]))   #ritorna l'ultimo dizionario aggiunto
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True,port=5005)
