7f7a287c0df3ec488873b1068dde674f085d327c
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-08-23 12:37:43 +0200

    aggiustate get dei dati

--- compute.py
+++ compute.py
@@ -7,6 +7,7 @@ import math
 from dotenv import load_dotenv
 from os import getenv
 
+NUM_SENSORI = 4
 URL_QUERY = "http://127.0.0.1:5003/query"
 DATI = []
 
@@ -19,32 +20,35 @@ app = Flask(__name__)
 def helloworld():
     return jsonify({"about": " Helloworld !"})
 
-@app.route('/calcoli/<int:id>', methods=[ 'GET','POST'])
-def calcoli(id):
-    row = requests.get(URL_QUERY+'/'+str(id))
-    riga = json.dumps(row.json())
-    chars = '[]'
-    res = riga.translate(str.maketrans('','',chars))
-    l = res.split(",")  #formattiamo la stringa levando i caratteri inutili
-    a = []
-    for i in range(len(l)):
-        a.append(float(l[i]))   #trasformiamo gli elementi della lista in float
+@app.route('/calcoli', methods=[ 'GET','POST'])
+def calcoli():
+    for id in range(NUM_SENSORI):
+        row = requests.get(URL_QUERY+'/'+str(id+1))
+        riga = json.dumps(row.json())
+        chars = '[]'
+        res = riga.translate(str.maketrans('','',chars))
+        l = res.split(",")  #formattiamo la stringa levando i caratteri inutili
+        a = []
+        for i in range(len(l)):
+            a.append(float(l[i]))   #trasformiamo gli elementi della lista in float
 
-    minimo = min(a)
-    massimo = max (a)
+        minimo = min(a)
+        massimo = max (a)
 
-    mean = sum(a) / len(a)
-    var = sum((l-mean)**2 for l in a) / len(a)
-    st_dev = math.sqrt(var)
+        mean = sum(a) / len(a)
+        var = sum((l-mean)**2 for l in a) / len(a)
+        st_dev = math.sqrt(var)
+        
+        print("Statistiche su sensore: ",id+1)
+        print("Standard deviation of the given list: " + str(st_dev))
+        print("media = ", mean)
 
-    print("Standard deviation of the given list: " + str(st_dev))
-    print("media = ", mean)
+        print ("min =",minimo)
+        print("max = ", massimo)
 
-    print ("min =",minimo)
-    print("max = ", massimo)
-
-    dati = {"id_sensore":id, "min":minimo, "max":massimo, "media":mean, "devs":st_dev }
-    DATI.append(dati)
+        dati = {"id_sensore":id+1, "min":minimo, "max":massimo, "media":mean, "devs":st_dev }
+        DATI.append(dati)
+    
     print(DATI)
 
     
